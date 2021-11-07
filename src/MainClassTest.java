2c79bfdb436fee5e418e8329ab4d92654d17397e
uniq-username <ilya-slivakov@yandex.ru>
2021-11-07 17:51:41 +0300

    test 3 commit, fixed class name

--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -3,7 +3,7 @@ import org.junit.Test;
 
 public class MainClassTest {
     @Test
-    public void testGetLocalNumber(){
+    public void testGetClassString(){
         MainClass mainClass = new MainClass();
         Assert.assertTrue("Function getClassString() is expected to contain the substring \"hello\" or \"Нello\"",
                 mainClass.getClassString().contains("hello") || mainClass.getClassString().contains("Hello"));
