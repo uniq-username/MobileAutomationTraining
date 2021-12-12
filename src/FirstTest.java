f8d3a129e00fe1c61f0546cc1e622311a1b5ef6e
uniq-username <ilya-slivakov@yandex.ru>
2021-12-13 00:53:12 +0300

    lesson 4, ex7. Added orientation to capabilities in setup

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -30,46 +30,27 @@ public class FirstTest {
         capabilities.setCapability("appActivity", ".main.MainActivity");
         capabilities.setCapability("app", "/Users/ilya-slivakov/Desktop/LearnQa/MobileAutomationTraining/apks/org.wikipedia.apk");
 
+        //Добавлена настройка оринтации
+        capabilities.setCapability("orientation", "PORTRAIT");
+
         driver = new AndroidDriver(new URL("http://0.0.0.0:4723/wd/hub"), capabilities);
+    }
+
+    private WebElement waitForElement(By by, long timeInSeconds){
+        WebDriverWait wait = new WebDriverWait(driver, timeInSeconds);
+        return wait.until(ExpectedConditions.presenceOfElementLocated(by));
     }
 
     @Test
     public void searchFieldContainsText()
     {
-//        //локатор строки поиска на главной странице по xpath
-//        By textField = By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.view.ViewGroup/android.support.v4.view.ViewPager/android.view.ViewGroup/android.widget.FrameLayout/android.support.v7.widget.RecyclerView/android.widget.FrameLayout[1]/android.widget.LinearLayout/android.widget.TextView");
-//        //локатор строки поиска на странице поиска по id
-//        By searchField = By.id("org.wikipedia:id/search_src_text");
-//        //локатор результатов поиска
-//        By searResults = By.id("org.wikipedia:id/page_list_item_title");
-//        //локатор первого результата поиска
-//        By firstSearchResult = By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.LinearLayout[1]/android.widget.FrameLayout[2]/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.ListView/android.widget.LinearLayout[1]/android.widget.LinearLayout/android.widget.TextView");
-//        //локатор кнопки сброса поиска
-//        By cancelSearchButton = By.id("org.wikipedia:id/search_close_btn");
-//        //локатор изображения страницы поиска без результатов
-//        By emptySearchImg = By.id("org.wikipedia:id/search_empty_image");
-//        //локатор текста страницы поиска без результатов
-//        By emptySearchText = By.id("org.wikipedia:id/search_empty_message");
-
-        driver.rotate(ScreenOrientation.LANDSCAPE);
+        waitForElement(By.xpath("//*[@text='Поиск по Википедии']"), 5).click();
+        waitForElement(By.xpath("//*[@text='Поиск']"), 5).sendKeys("Java");
     }
 
     @After
     public void tearDawn()
     {
-        //Проверяем ориентацию экрана и если она не портретная то меняем
-        if (driver.getOrientation() != ScreenOrientation.PORTRAIT){
-            driver.rotate(ScreenOrientation.PORTRAIT);
-        }
-        driver.quit();
-    }
-
-    private void assertElementHasText(By by, String expectedText, String errorText){
-        WebElement element = waitForElement(by, 5);
-        Assert.assertTrue(errorText, element.getText().contains(expectedText));
-    }
-    private WebElement waitForElement(By by, long timeInSeconds){
-        WebDriverWait wait = new WebDriverWait(driver, timeInSeconds);
-        return wait.until(ExpectedConditions.presenceOfElementLocated(by));
+        //driver.quit();
     }
 }
